43cc3f29f2d3ce26a4c0883fe4be4b4a1fb2b410
Clemens Lange <clemens.lange@cern.ch>
2016-06-30 12:10:26 +0200

    additions to submit MiniAODv1

--- DIGI-RECO/submitCrab_MiniAODv1.sh
+++ DIGI-RECO/submitCrab_MiniAODv1.sh
@@ -1,17 +1,18 @@
 #!/bin/zsh
 
 NJOBS=500
-CRABTEMPLATE=crabConfig_MCgeneration_RECO76X_step2.py
+CRABTEMPLATE=crabConfig_MCgeneration_MiniAODv1.py
 DATE=`date +'%F'`
 
-for DATASETIN in `cat datasets_DIGI.txt`; do
+for DATASETIN in `cat datasets_AODSIM.txt`; do
   echo "DATASETIN: $DATASETIN"
   a=("${(@s/-/)DATASETIN}")
   INSHORT=$a[2]-$a[3]
+  echo "INSHORT $INSHORT"
   CRABCONFIG=crabConfig_${INSHORT}.py
   echo "CRABCONFIG $CRABCONFIG"
-  DATASETOUT=${INSHORT}_RECO76X
-  echo "DATASETOUT: $DATASETOUT"
+  DATASETOUT=${INSHORT}_MiniAODv1_76X
+   echo "DATASETOUT: $DATASETOUT"
   cp $CRABTEMPLATE $CRABCONFIG
   sed -i -e "s|DATASETIN|$DATASETIN|g" $CRABCONFIG
   sed -i -e "s|INSHORT|$INSHORT|g" $CRABCONFIG
@@ -19,4 +20,5 @@ for DATASETIN in `cat datasets_DIGI.txt`; do
   sed -i -e "s/DATE/$DATE/g" $CRABCONFIG
   sed -i -e "s/NJOBS/$NJOBS/g" $CRABCONFIG
   echo crab submit -c $CRABCONFIG
+  break
 done
