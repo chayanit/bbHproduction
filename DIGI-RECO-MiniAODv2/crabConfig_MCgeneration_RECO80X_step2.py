662023e1b6007406daaa6e668d1b3ba99c2ed2bf
Clemens Lange <clemens.lange@cern.ch>
2016-07-23 23:56:56 +0200

    clean up 76X

--- DIGI-RECO-MiniAODv2/crabConfig_MCgeneration_RECO80X_step2.py
+++ DIGI-RECO-MiniAODv2/crabConfig_MCgeneration_RECO80X_step2.py
@@ -1,14 +1,14 @@
 from CRABClient.UserUtilities import config, getUsernameFromSiteDB
 config = config()
 
-config.General.requestName = 'request_RECO76X_INSHORT_DATE'
+config.General.requestName = 'request_RECO80X_INSHORT_DATE'
 config.General.workArea = 'crab_projects'
 config.General.transferOutputs = True
 config.General.transferLogs = True
 
 # config.JobType.pluginName = 'PrivateMC'
 config.JobType.pluginName = 'Analysis'
-config.JobType.psetName = 'config_RECO76X_step2.py'
+config.JobType.psetName = 'config_RECO80X_step2.py'
 
 config.Data.inputDataset = 'DATASETIN'
 config.Data.inputDBS = 'phys03'
